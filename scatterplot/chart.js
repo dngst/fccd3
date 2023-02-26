42458690323dcfb64bac023647be2bac6aba2402
dngst <dngst@protonmail.com>
2023-02-26 23:12:20 +0300

    Add scatterplot

---
+++ scatterplot/chart.js
@@ -0,0 +1,118 @@
+async function drawScatterPlot() {
+  const dataset = await d3.json("../nyc_weather_data.json")
+
+  const xAccessor = d => d.dewPoint
+  const yAccessor = d => d.humidity
+  const colorAccessor = d => d.cloudCover
+
+  const width = d3.min([
+    window.innerWidth * 0.9,
+    window.innerHeight * 0.9
+  ])
+
+  let dimensions = {
+    width: width,
+    height: width,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 50,
+      left: 50
+    }
+  }
+
+  dimensions.boundedWidth = dimensions.width
+      - dimensions.margin.left
+      - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height
+      - dimensions.margin.top
+      - dimensions.margin.bottom
+
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
+
+  const bounds = wrapper.append("g")
+    .style("transform", `translate(${
+      dimensions.margin.left
+    }px, ${
+      dimensions.margin.top
+    }px)`)
+
+  const colorScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, colorAccessor))
+    .range(["skyblue", "darkslategrey"])
+
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice()
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .range([dimensions.boundedHeight, 0])
+    .nice()
+
+  bounds.append("circle")
+    .attr("cx", dimensions.boundedWidth / 2)
+    .attr("cy", dimensions.boundedHeight / 2)
+    .attr("r", 5)
+
+  const dots = bounds.selectAll("circle")
+      .data(dataset)
+    .enter().append("circle")
+      .attr("cx", d => xScale(xAccessor(d)))
+      .attr("cy", d => yScale(yAccessor(d)))
+      .attr("r", 5)
+      .attr("fill", d => colorScale(colorAccessor(d)))
+
+  const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+
+  const xAxis = bounds.append("g")
+    .call(xAxisGenerator)
+      .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+
+  const yAxisGenerator = d3.axisLeft()
+    .scale(yScale)
+
+  const yAxis = bounds.append("g")
+    .call(yAxisGenerator)
+
+  const yAxisLabel = yAxis.append("text")
+    .attr("x", -dimensions.boundedHeight / 2)
+    .attr("y", -dimensions.margin.left + 15)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .text("Relative humidity")
+    .style("transform", "rotate(-90deg)")
+    .style("text-anchor", "middle")
+
+  const xAxisLabel = xAxis.append("text")
+    .attr("x", dimensions.boundedHeight / 2)
+    .attr("y", dimensions.margin.top + 25)
+    .attr("fill", "black")
+    .style("font-size", "1.4em")
+    .text("Dew point (F)")
+
+  // voronoi
+  const delaunay = d3.Delaunay.from(
+    dataset,
+    d => xScale(xAccessor(d)),
+    d => yScale(yAccessor(d))
+  )
+
+  const voronoi = delaunay.voronoi()
+  voronoi.xmax = dimensions.boundedWidth
+  voronoi.ymax = dimensions.boundedHeight
+
+  bounds.selectAll(".voronoi")
+    .data(dataset)
+    .enter().append("path")
+      .attr("d", (d, i) => voronoi.renderCell(i))
+      .attr("stroke", "salmon")
+      .style("fill", "none")
+}
+
+drawScatterPlot()
