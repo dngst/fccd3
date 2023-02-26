ac34eeff6f4213c3ade94b41787a1b0c383f6dc0
dngst <dngst@protonmail.com>
2023-02-26 23:12:02 +0300

    Add linechart

---
+++ linechart/chart.js
@@ -0,0 +1,81 @@
+async function drawLineChart() {
+  const dataset = await d3.json("../nyc_weather_data.json")
+  const dateParser = d3.timeParse("%Y-%m-%d")
+
+  const yAccesor = d => d.temperatureMax
+  const xAccesor = d => dateParser(d.date)
+
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margin: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60
+    }
+  }
+
+  dimensions.booundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom
+
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
+
+  let bounds = wrapper.append("g")
+    .style("transform", `translate(${
+      dimensions.margin.left
+    }px, ${
+      dimensions.margin.top
+    }px)`)
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(dataset, yAccesor))
+    .range([dimensions.boundedHeight, 0])
+
+  const freezingTemperaturePlacement = yScale(32)
+  const freezingTemperatures = bounds.append("rect")
+    .attr("x", 0)
+    .attr("width", dimensions.booundedWidth)
+    .attr("y", freezingTemperaturePlacement)
+    .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
+    .attr("fill", "#E0F3F3")
+
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(dataset, xAccesor))
+    .range([0, dimensions.booundedWidth])
+
+  const lineGenerator = d3.line()
+    .curve(d3.curveBasis)
+    .x(d => xScale(xAccesor(d)))
+    .y(d => yScale(yAccesor(d)))
+
+  const line = bounds.append("path")
+    .attr("d", lineGenerator(dataset))
+    .attr("fill", "none")
+    .attr("stroke", "#AF9358")
+    .attr("stroke-width", 2)
+
+  const yAxisGenerator = d3.axisLeft()
+    .scale(yScale)
+
+  const yAxis = bounds.append("g")
+    .call(yAxisGenerator)
+
+  const xAxisGenerator = d3.axisBottom()
+    .scale(xScale)
+
+  const xAxis = bounds.append("g")
+    .call(xAxisGenerator)
+      .style("transform", `translateY(${
+        dimensions.boundedHeight
+      }px)`)
+}
+
+drawLineChart()
