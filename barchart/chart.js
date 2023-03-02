96da6000b0274ca34cf7cead9ee54f7511ebf778
dngst <dngst@protonmail.com>
2023-03-02 10:38:32 +0300

    Draw data

--- barchart/chart.js
+++ barchart/chart.js
@@ -4,13 +4,13 @@ async function drawBarChart() {
     return response.data
   })
 
-  const width = 600
+  const width = 900
   let dimensions = {
     width: width,
     height: width * 0.6,
     margin: {
       top: 30,
-      right: 10,
+      right: 20,
       bottom: 50,
       left: 50
     }
@@ -37,10 +37,13 @@ async function drawBarChart() {
 
   const years = []
   const gdps = []
+  const filtredData =[]
 
   for (const row of dataset) {
+    const year = new Date(row[0]).getFullYear()
+    filtredData.push([year, row[1]])
     for(const either of row) {
-      years.push(new Date(row[0]).getFullYear())
+      years.push(year)
       gdps.push(row[1])
     }
   }
@@ -53,8 +56,10 @@ async function drawBarChart() {
     .scale(xScale)
 
   const xAxis = bounds.append("g")
-  .call(xAxisGenerator)
-    .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+    .call(xAxisGenerator)
+      .attr("id", "x-axis")
+      .style("transform", `translateY(${dimensions.boundedHeight}px)`)
+      .attr("class", "tick")
 
   const yScale = d3.scaleLinear()
     .domain(d3.extent(gdps))
@@ -66,6 +71,21 @@ async function drawBarChart() {
 
   const yAxis = bounds.append("g")
     .call(yAxisGenerator)
+      .attr("id", "y-axis")
+      .attr("class", "tick")
+
+  const barPadding = 1
+  const binsGroup = bounds.append("g")
+  const barRects = binsGroup.selectAll("g")
+    .data(filtredData)
+    .enter().append("rect")
+    .attr("data-date", d => d[0])
+    .attr("data-gdp", d => d[1])
+    .attr("x", d => xScale(d[0]))
+    .attr("y", d => yScale(d[1]))
+    .attr("width", 18)
+    .attr("height", d => dimensions.boundedHeight - yScale(d[1]))
+    .attr("class", "bar")
 }
 
 drawBarChart()
